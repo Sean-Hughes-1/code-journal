1d3b41f08bf761a2e043d8ba3a232837af57ab28
Sean Hughes <coronapilot69@gmail.com>
2022-12-28 19:55:49 +0000

    Sean Hughes - completed issue-1-create-entry

--- js/data.js
+++ js/data.js
@@ -6,3 +6,12 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+var previousData = localStorage.getItem('javascript-local-storage');
+window.addEventListener('beforeunload', handleBeforeunload);
+function handleBeforeunload(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataJSON);
+}
+if (previousData !== null) {
+  previousData = JSON.parse(previousData);
+}
